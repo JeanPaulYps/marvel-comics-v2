8bfb688d413bb7fd0fbf60afb29b3929fe7667b6
Jean Paul <paul.yepesg@perficient.com>
2021-10-07 12:37:38 -0500

    actulizacion header

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -3,9 +3,11 @@ import '../styles/Header.css';
 
 function Header() {
     return (
+        <>
         <div class="Header__Container">
             <img src={`${process.env.PUBLIC_URL}/MarvelLogo.svg`} alt="" class="Header__Logo" />
         </div>
+        </>
     )
 }
 
