a4c5a78e2843781db4bd2061e3a3f1d8aef734da
Jean Paul <paul.yepesg@perficient.com>
2021-10-08 11:48:16 -0500

    Se crea el layout base y los estilos de los ultimos comics disponibles

--- src/containers/LastComics.jsx
+++ src/containers/LastComics.jsx
@@ -4,14 +4,17 @@ import '../styles/LastComics.css'
 function LastComics() {
     return (
         <div className="LastComics">
-            <div className="LastComics__Item"></div>
-            <div className="LastComics__Item"></div>
-            <div className="LastComics__Item"></div>
-            <div className="LastComics__Item"></div>
-            <div className="LastComics__Item"></div>
-            <div className="LastComics__Item"></div>
+            <div className="LastComics__Item">
+                <img src={process.env.PUBLIC_URL + "/ExampleComic.jpg"} alt="" className="LastComics__Cover" />
+                <button type="button" className="AddToCartButton">
+                        <img src={`${process.env.PUBLIC_URL}/ShoppingcartInButton.svg`}  alt="" className="AddToCartButton__CartIcon" />
+                        <span> ADD TO CART </span>
+                </button>
+                <p className="LastComics__Title">Titulo Comic</p>
+                <p className="LastComics__Author">Hickman, Schiti</p>
+            </div>
         </div>
     )
 }
 
-export {LastComics, }
+export {LastComics, }   
